4b209c36473338b68edbb473be7ee67fe12d2792
musarratChowdhury <muhitlopez143@gmail.com>
2024-09-12 10:30:37 +0600

    testing object detection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import * as tf from "@tensorflow/tfjs";
-const dataArray = [8, 6, 7, 5, 3, 0, 9];
-const first = tf.tensor(dataArray);
-console.log(first);
-const first_again = tf.tensor1d(dataArray);
-// const guess = tf.tensor2d([true, false, false], undefined, "int32");
-const d = tf.tensor([1, 0, 0, 0, -1, 0, 1, 0, 0], [3, 3], "int32");
-console.log(d);
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+
+let modelPromise;
+
+window.onload = async () => {
+  const img = document.getElementById("img") as HTMLImageElement;
+  modelPromise = cocoSsd.load();
+  const model = await modelPromise;
+  console.log(model);
+  const predictions = await model.detect(img);
+  console.log(predictions);
+};
